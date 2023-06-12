fdd33ffaac4086020c3342e7367afc373e49604c
ychento <your@email.comyiyaochen@gmail.com>
2023-06-12 02:59:55 +0000

    edit wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,23 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map((ls) => ls.join(''));
+    const verticalJoin = transpose(letters).map((ls) => ls.join(''));
+  
+    for (const l of horizontalJoin) {
+      if (l.includes(word)) return true;
     }
-}
+  
+    for (const l of verticalJoin) {
+      if (l.includes(word)) return true;
+    }
+  
+    return false;
+  };
+
+// Helper function to transpose the matrix
+const transpose = (matrix) => {
+    return matrix[0].map((_, columnIndex) => matrix.map((row) => row[columnIndex]));
+  };
+
+
 
 module.exports = wordSearch
